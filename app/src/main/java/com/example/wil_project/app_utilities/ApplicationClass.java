b7eca3986c79b05133cc310ee3c403508469eab7
NtsenoOnGitHub <52259757+NtsenoOnGitHub@users.noreply.github.com>
2022-01-10 21:54:09 +0200

    Updated Application Class

--- app/src/main/java/com/example/wil_project/app_utilities/ApplicationClass.java
+++ app/src/main/java/com/example/wil_project/app_utilities/ApplicationClass.java
@@ -9,7 +9,7 @@ public class ApplicationClass extends Application {
     public static final String APPLICATION_ID = "FD83BCFC-EBBD-E8F5-FF55-5925171AF400";
     public static final String API_KEY = "C86343FC-EF81-49CF-9120-5658A39CF76D";
     public static int HitchhikerId;
-    public static int remoteCheck;
+    public static int remote_Check;
 
     @Override
     public void onCreate() {
